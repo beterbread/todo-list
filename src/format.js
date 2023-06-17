1f128903582f8b210b518a47edccdcd37e20c48d
Peter Kim <petertimmmybob113@gmail.com>
2023-06-17 16:48:15 -0500

    Side

--- src/format.js
+++ src/format.js
@@ -13,6 +13,26 @@ const format = () => {
 
   header.append(headerText);
   container.append(header);
+
+  /* Side */
+  const side = document.createElement('div');
+  const tasks = document.createElement('p');
+  const today = document.createElement('p');
+  const week = document.createElement('p');
+  const projects = document.createElement('p');
+  side.classList.add('side');
+
+  tasks.textContent = 'Home';
+  today.textContent = 'Today';
+  week.textContent = 'Week';
+  projects.textContent = 'Projects';
+  
+  side.append(tasks);
+  side.append(today);
+  side.append(week);
+  side.append(projects);
+  container.append(side);
+
 };
 
 export { format };
